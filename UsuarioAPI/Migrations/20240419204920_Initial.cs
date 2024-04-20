9bd50fc9ccb2032eb0ece6ae658026cff866002a
Sebastian Naranjo <sebas2013n@gmail.com>
2024-04-19 21:37:57 -0500

    CRUD

---
+++ UsuarioAPI/Migrations/20240419204920_Initial.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace UsuarioAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class Initial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    primer_nombre = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    segundo_nombre = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: true),
+                    primer_apellido = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    segundo_apellido = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: true),
+                    fecha_nacimiento = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    sueldo = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    fecha_creacion = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    fecha_modificacion = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Users");
+        }
+    }
+}
---
+++ UsuarioAPI/Models/User.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace UsuarioAPI.Models
+{
+    public class User
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int id { get; set; }
+
+        [StringLength(50)]
+        [Required(ErrorMessage = "Debe ingresar el primer nombre")]
+        public required string primer_nombre { get; set; }
+
+        [StringLength(50)]
+        public string segundo_nombre { get; set; } = string.Empty;
+
+        [StringLength(50)]
+        [Required(ErrorMessage = "Debe ingresar el primer apellido")]
+        public required string primer_apellido { get; set; }
+
+        [StringLength(50)]
+        public string segundo_apellido { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "Debe ingresar la fecha de nacimiento")]
+        public required DateTime fecha_nacimiento { get; set; }
+
+        [Required(ErrorMessage = "El sueldo es obligatorio")]
+        [Range(0.01, double.MaxValue, ErrorMessage = "El sueldo debe ser mayor que 0")]
+        public required decimal sueldo { get; set; }
+
+        public DateTime fecha_creacion { get; set; }
+
+        public DateTime fecha_modificacion { get; set; }
+
+    }
+}
